28108f97766499bc54e1ed94c83a8e3bb9e90464
zloyvetal <zloyvetal@gmail.com>
2017-07-14 17:45:40 +0300

    add date_time and opportinity to save data as JSON

--- src/create_local_db.py
+++ src/create_local_db.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
-from sqlalchemy import Table, Column, Integer, String, MetaData
-from sqlalchemy.orm import mapper
+from sqlalchemy import Table, Column, Integer, String, DateTime
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import datetime
+from sqlalchemy.dialects.postgresql import JSON
+import json
 
 engine = create_engine("postgres://postgres:postgres@localhost:5432/upwork_data")
 if not database_exists(engine.url):
@@ -12,27 +15,37 @@ if not database_exists(engine.url):
 else:
     print("DB Already exist")
 
-metadata = MetaData()
-table_name = "upwork_info"
-upwork_info_table = Table(table_name, metadata,
-                          Column('id', Integer, primary_key=True),
-                          Column('raw_data', String(500))
-                          )
-metadata.create_all(engine)
-print("table %s created" % table_name)
+
+Base = declarative_base()
+
+# table_name = "upwork_info"
+# upwork_info_table = Table(table_name, metadata,
+#                           Column('id', Integer, primary_key=True),
+#                           Column('raw_data', String(500)),
+#                           Column('date_time', DateTime )
+#                           )
+# metadata.create_all(engine)
+# print("table %s created" % table_name)
 
 
-class UpworkInfo(object):
-    def __init__(self, raw_data):
-        self.raw_data = raw_data
+class UpworkInfo(Base):
+    __tablename__ = "upwork_info"
+    id = Column(Integer, primary_key=True)
+    raw_data = Column(JSON, nullable=False)
+    added_time = Column(DateTime, default=datetime.datetime.utcnow)
 
-mapper(UpworkInfo, upwork_info_table)
 
-Session = sessionmaker(bind=engine)
+Session = sessionmaker()
+Session.configure(bind=engine)
 session = Session()
+
 print("Session is created successful")
 
+Base.metadata.create_all(engine)
+
+print("Table is created successful")
 
 def populate_db(data):
-    session.add_all([UpworkInfo(i) for i in data])
+    session.add_all([UpworkInfo(raw_data=json.loads(i)) for i in data])
     session.commit()
+
