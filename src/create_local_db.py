916cdb5884f460ae2aebf236bd46ad9304e7af4b
zloyvetal <zloyvetal@gmail.com>
2017-07-14 13:20:05 +0300

    minor refactor

--- src/create_local_db.py
+++ src/create_local_db.py
@@ -7,31 +7,32 @@ from sqlalchemy.orm import sessionmaker
 engine = create_engine("postgres://postgres:postgres@localhost:5432/upwork_data")
 if not database_exists(engine.url):
     create_database(engine.url)
-
-print(database_exists(engine.url))
+    if database_exists(engine.url):
+        print("DB is created")
+else:
+    print("DB Already exist")
 
 metadata = MetaData()
-users_table = Table('users', metadata,
-                    Column('id', Integer, primary_key=True),
-                    Column('user_info', String(500))
-                    )
+table_name = "upwork_info"
+upwork_info_table = Table(table_name, metadata,
+                          Column('id', Integer, primary_key=True),
+                          Column('raw_data', String(500))
+                          )
 metadata.create_all(engine)
+print("table %s created" % table_name)
 
 
-class User(object):
+class UpworkInfo(object):
+    def __init__(self, raw_data):
+        self.raw_data = raw_data
 
-    def __init__(self, user_info):
-        self.user_info = user_info
-
-mapper(User, users_table)
+mapper(UpworkInfo, upwork_info_table)
 
 Session = sessionmaker(bind=engine)
 session = Session()
+print("Session is created successful")
 
 
 def populate_db(data):
-    session.add_all([User(i) for i in data])
-
-session.commit()
-
-
+    session.add_all([UpworkInfo(i) for i in data])
+    session.commit()
