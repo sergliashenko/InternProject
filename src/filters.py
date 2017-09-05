d4e7bfe3c7f3807641b2520805d89a30dac899bb
segliashenko <segliashenko@gmail.com>
2017-09-05 18:06:50 +0300

    small fixes

--- src/filters.py
+++ src/filters.py
@@ -1,5 +1,6 @@
 from src import feature
 
+
 def check_client_country(json_data):
     """
     If client from India, Turkey, than return True
@@ -8,10 +9,11 @@ def check_client_country(json_data):
     """
     excluded_country = ["India", "Turkey"]
     client_info = feature.get_client_info(json_data)
-    if client_info != None:
+    if client_info is not None:
         if excluded_country in client_info:
             return True
     return False
+
 
 def check_vacancies_which_only_for_registered_users(json_data):
     """
@@ -25,6 +27,7 @@ def check_vacancies_which_only_for_registered_users(json_data):
     else:
         return False
 
+
 def check_smollest_price(json_data):
     """
     If avg price per hour is very smallest then return True, in other case False
@@ -32,10 +35,11 @@ def check_smollest_price(json_data):
     :return: bool
     """
     avg_hourly_rate = feature.get_avg_hourly_rate(json_data)
-    if avg_hourly_rate != None:
+    if avg_hourly_rate is not None:
         if avg_hourly_rate < 3.0:
             return True
     return False
+
 
 def filter(json_data):
     if check_vacancies_which_only_for_registered_users(json_data):
@@ -43,12 +47,12 @@ def filter(json_data):
     if check_client_country(json_data) and check_smollest_price(json_data):
         return False
     hires = feature.get_client_history(json_data)
-    if hires != None and hires == 0 and check_smollest_price(json_data):
+    if hires is not None and hires == 0 and check_smollest_price(json_data):
         return False
-    posted_time = feature.get_job_posted_time(json_data)
+    posted_time = feature.get_posted_time(json_data)
     #TODO expected function get_proposals from Vitalik
     proposals = 0
-    if posted_time != None and posted_time > 3.0 and proposals == "Less than 3":
+    if posted_time is not None and posted_time > 3.0 and proposals == "Less than 3":
         return False
     return True
 
