06bab10af9fbbfc3324c7d0fc3e5115ca80371e2
vlad <vl.khizanov@gmail.com>
2017-10-02 19:15:48 +0300

    model pickling added

--- src/model.py
+++ src/model.py
@@ -1,16 +1,14 @@
 from src import encoder
 
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
-
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import KFold
 from sklearn.model_selection import GridSearchCV
+from sklearn.externals import joblib
 
 import numpy as np
 
 import os
 import json
+
 from typing import Tuple
 
 
@@ -28,50 +26,37 @@ def prepare_data(path: str) -> Tuple[np.ndarray, np.ndarray]:
 
                 features.append(feature[0])
                 labels.append(label[0])
+
     return np.array(features), np.array(labels)
 
 
-def cross_validate(features: np.ndarray, labels: np.ndarray) -> None:
-    rf = RandomForestClassifier(n_estimators=50, max_features="log2", random_state=50, n_jobs=-1,
-                                class_weight="balanced")
-    parameters_grid = {
-        "n_estimators": [100, 300, 500, 700],
-        "max_features": ['sqrt', 'log2'],
-        "random_state": [40, 60, 80, 100]
-    }
-    # K-Fold cross validation
-    cv = KFold(n_splits=10, shuffle=True)
-    cv_rf = GridSearchCV(estimator=rf, param_grid=parameters_grid, cv=cv, verbose=1, n_jobs=-1)
+def cross_validate(features: np.ndarray, labels: np.ndarray, n_folds: int=10):
+    rf = RandomForestClassifier(n_jobs=-1, class_weight="balanced")
+    parameters_grid = {"n_estimators": [100, 300, 500, 700],
+                       "max_features": ['sqrt', 'log2']}
 
-    for train_idx, test_idx in cv.split(features):
-        X_train, X_test = features[train_idx], features[test_idx]
-        y_train, y_test = labels[train_idx], labels[test_idx]
-        cv_rf.fit(X_train, y_train)
-        y_pred = cv_rf.predict(X_test)
+    cv_rf = GridSearchCV(estimator=rf, param_grid=parameters_grid, scoring="f1", cv=n_folds, verbose=2, n_jobs=-1)
+    cv_rf.fit(features, labels)
+    print(cv_rf.cv_results_)
 
-        print("best params: ", cv_rf.best_params_)
-        print("best score: ", cv_rf.best_score_)
-
-        print("y_test: %i\ty_pred: %i" % (sum(y_test), sum(y_pred)))
-        report = classification_report(y_test, y_pred)
-        print(report)
+    return cv_rf.best_estimator_
 
 
-def train(features: np.ndarray, labels: np.ndarray) -> None:
-    rf = RandomForestClassifier(200)
+def train(features: np.ndarray, labels: np.ndarray, model_params):
+    rf = RandomForestClassifier(**model_params)
+    rf.fit(features, labels)
+    return rf
 
-    X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.33)
-    rf.fit(X_train, y_train)
 
-    y_pred = rf.predict(X_test)
+def save_model(model, path):
+    joblib.dump(model, path)
 
-    print("y_test: %i\ty_pred: %i" % (sum(y_test), sum(y_pred)))
-    print(classification_report(y_test, y_pred))
+
+def load_model(path):
+    return joblib.load(path)
 
 
 if __name__ == '__main__':
-    features, labels = prepare_data(r"D:\ParserUpWork\src\resources\data")
-    # train(features, labels)
-    cross_validate(features=features, labels=labels)
-    # for i in range(5):
-    #     train(features, labels)
+    features, labels = prepare_data(os.path.join("resources", "data"))
+    model = cross_validate(features=features, labels=labels)
+    save_model(model, os.path.join("resources", "data", "model.pkl"))
